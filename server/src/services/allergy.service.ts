970cb9ed06e73a5b849ffd46da9f0dfa21f96e1b
Ashin2052 <mahat.ashin@gmail.com>
2023-02-07 09:45:03 +0545

    LFUNIOLD-88: Logger update

--- server/src/services/allergy.service.ts
+++ server/src/services/allergy.service.ts
@@ -1,5 +1,6 @@
 import AllergySchema, {IAllergy} from '../models/allergy.schema';
 import {Ipagination} from '../types/shared.types';
+import logger from '../configs/logger';
 
 const cloudinary = require('../configs/cloudinary.config');
 
@@ -37,6 +38,7 @@ export const createAllergy = async (allergy: IAllergy, file: any): Promise<any> 
         allergy.image = result.secure_url;
         return AllergySchema.create(allergy);
     } catch (err) {
+        logger.error(`Create allergy error: ${allergy.name}`);
         throw err;
     }
 }
@@ -48,13 +50,14 @@ export const createAllergy = async (allergy: IAllergy, file: any): Promise<any> 
  */
 export const deleteAllergy = async (id: string): Promise<any> => {
     try {
-         await AllergySchema.findByIdAndDelete(id).then(value => {
+        await AllergySchema.findByIdAndDelete(id).then(value => {
                 cloudinary.uploader.destroy(value.cloudinaryId);
             }
         );
 
         return id;
     } catch (err) {
+        logger.error(`Delete allergy error: ${id}`);
         throw err;
     }
 }
@@ -67,10 +70,10 @@ export const deleteAllergy = async (id: string): Promise<any> => {
  * @param {object} file
  * @return {promise}
  */
-export const updateAllergy = async (allergy: IAllergy,id:string, file: any): Promise<any> => {
+export const updateAllergy = async (allergy: IAllergy, id: string, file: any): Promise<any> => {
     try {
 
-        const oldAllergy:any = await AllergySchema.findById(id).lean();
+        const oldAllergy: any = await AllergySchema.findById(id).lean();
         // Delete image from cloudinary
         if (file) {
             await cloudinary.uploader.destroy(oldAllergy?.cloudinaryId);
@@ -78,16 +81,16 @@ export const updateAllergy = async (allergy: IAllergy,id:string, file: any): Pro
             const result = await cloudinary.uploader.upload(file.path);
             allergy.cloudinaryId = result.public_id;
             allergy.image = result.secure_url;
-        }
-        else {
+        } else {
             allergy.cloudinaryId = oldAllergy.cloudinaryId;
             allergy.image = oldAllergy.secure_url;
 
         }
-        return  AllergySchema.findByIdAndUpdate(id, allergy, {
+        return AllergySchema.findByIdAndUpdate(id, allergy, {
             new: true
         });
     } catch (err) {
+        logger.error(`Update allergy error: ${allergy.name}`);
         throw err
     }
 }
